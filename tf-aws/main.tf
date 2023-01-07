12a957435abbf16c0d03bb1f56de255539d38d64
l00177583 <l00177583@atu.ie>
2023-01-07 23:55:36 +0000

    Udpate Terraforrm and git ignore file

---
+++ tf-aws/main.tf
@@ -0,0 +1,32 @@
+provider "aws" {
+  region = "us-east-1"
+  //version = "~>4.48.0"
+}
+
+resource "aws_instance" "Web" {
+  ami             = "ami-0b5eea76982371e91"
+  instance_type   = "t2.micro"
+  key_name        = "mp1ubuntu"
+  security_groups = [aws_security_group.web.name]
+  tags = {
+    name = "WebServerbyTf"
+  }
+}
+
+resource "aws_security_group" "web" {
+  name        = "web-security-group"
+  description = "allow access to web user"
+
+  ingress {
+    description = "allow ssh"
+    to_port     = 22
+    from_port   = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
+output "instance_public_dns" {
+  value = aws_instance.Web.public_dns
+}
